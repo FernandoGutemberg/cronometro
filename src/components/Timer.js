a342011dfe5112e0174110e9deda138d1c8373e6
FernandoGutemberg <fernando70sil@gmail.com>
2024-07-29 00:54:44 -0300

    Versão 3 do projeto cronometro, adicao de estilos

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,7 +1,18 @@
 import React from "react";
+import TimerDisplay from "./TimerDisplay";
+import TimerControls from "./TimerControls";
+import LapList from "./LapList";
+
+import "./Timer.css";
 
 const Timer = () => {
-    return <div></div>;
+    return (
+        <div className="timer-container">
+            <TimerDisplay />
+            <TimerControls />
+            <LapList />
+        </div>
+    );
 };
 
 export default Timer;
