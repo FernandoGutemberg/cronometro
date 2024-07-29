bb673174e487401db0831ec843f6ff799569082e
FernandoGutemberg <fernando70sil@gmail.com>
2024-07-28 23:28:51 -0300

    Versão 2 do projeto cronometro, remocao e adicao de files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Timer from './components/Timer';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+    <Timer/> 
+    
+    </>
+
   );
 }
 
